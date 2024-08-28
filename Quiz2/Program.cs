9f809cfa789703bf6612e5dc180de395ba9fff9d
Lancus Maxwell <youngsorcerer23@gmail.com>
2024-08-28 12:42:23 -0500

    Add project files.

---
+++ Quiz2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prompts user to enter item price, sales tax, and promotion discount
+            Console.WriteLine("Item Cost Calculator (Enter a decimal for percentage. 0.05 as 5%)");
+            Console.WriteLine("Enter the item's price: ");
+            float item = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the sales tax: ");
+            float tax = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the discount: ");
+            float discount = float.Parse(Console.ReadLine());
+            //Calculates the item's cost by 
+            float cost = item * tax / discount;
+            //displays answer of total cost
+            Console.WriteLine("The item's total cost is: " + cost + " dollars.");
+            Console.Read();
+        }
+    }
+}
